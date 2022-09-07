249e5547698126c19d6c3bdbe45d7c10bfcee52e
martirosharutyunyan <martiros.harutunyan@gmail.com>
2022-09-07 18:57:10 +0400

    fixed error handling

--- src/main.ts
+++ src/main.ts
@@ -10,30 +10,34 @@ const userId = Number(process.env.USER_ID!);
 const bot = new TelegramBot(BOT_TOKEN!);
 
 bot.on('message', (msg) => {
-    if (msg.chat.id !== userId) {
-        return;
-    }
-    const url = msg.text!.split('?')[0];
-    const paths = url.split('/');
-    const fileName = paths[paths.length - 1];
-    const adapter = url.startsWith('https') ? https: http;
-    adapter.get(url, (res) => {
-        // Image will be stored at this path
-        const path = `./files/${fileName}`;
-        const filePath = fs.createWriteStream(path);
-        res.pipe(filePath);
-        filePath.on('finish',async () => {
-            filePath.close();
-            console.log('Download Completed');
-            const file = fs.readFileSync(path);
-            await bot.sendVideo(msg.chat.id, file)
-            try {
-                fs.unlinkSync(path);
-            } catch (e) {
-                console.log(e)
-            }
+    try {
+        if (msg.chat.id !== userId) {
+            return;
+        }
+        const url = msg.text!.split('?')[0];
+        const paths = url.split('/');
+        const fileName = paths[paths.length - 1];
+        const adapter = url.startsWith('https') ? https: http;
+        adapter.get(url, (res) => {
+            // Image will be stored at this path
+            const path = `./files/${fileName}`;
+            const filePath = fs.createWriteStream(path);
+            res.pipe(filePath);
+            filePath.on('finish',async () => {
+                filePath.close();
+                console.log('Download Completed');
+                const file = fs.readFileSync(path);
+                await bot.sendVideo(msg.chat.id, file)
+                try {
+                    fs.unlinkSync(path);
+                } catch (e) {
+                    console.log(e)
+                }
+            })
         })
-    })
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 void bot.startPolling();
