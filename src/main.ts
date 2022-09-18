f7abf042ed985057737798b3a64b4843a58fd9d0
Martiros Harutyunyan <65274148+martirosharutyunyan@users.noreply.github.com>
2022-09-18 10:13:39 +0400

    added percentage showing when download and update video

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,10 @@ dotenv.config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const PASSWORD_HASH = process.env.PASSWORD_HASH!;
 const TELEGRAM_BASE_URL = process.env.TELEGRAM_BASE_URL;
+const ADMIN_ID = Number(process.env.ADMIN_ID);
 const bot = new TelegramBot(BOT_TOKEN!, { baseApiUrl: TELEGRAM_BASE_URL });
 
-const users: UserEntity[] = [];
+const users: UserEntity[] = [new UserEntity(ADMIN_ID, 'Martiros')];
 
 bot.on('message', async (msg) => {
     const text = msg.text!;
@@ -55,22 +56,52 @@ bot.on('message', async (msg) => {
     const adapter = url.startsWith('https') ? https : http;
     try {
         adapter.get(url, async (res) => {
-            if (res.statusCode! >= 300) {
+            if (res.statusCode! >= 400) {
                 await bot.sendMessage(userId, `Failed code: ${res.statusCode!}`)
                 return;
             }
-            
+
             const path = `./files/${fileName}`;
             const fileStream = fs.createWriteStream(path);
-
+            
+            const fileSize = Number(res.headers['content-length']);
+            let downloadedSize = 0;
+            let percentage = 10;
             res.pipe(fileStream);
+            const getPercentage = (downloadedSize, fileSize) => Math.floor((downloadedSize / fileSize) * 100);
+            const message = await bot.sendMessage(userId, `Downloaded: ${getPercentage(downloadedSize, fileSize)}%`);
+            const editMessageOptions = { message_id: message.message_id, chat_id: userId }
+            res.on('data', async (chunk) => {
+                const chunkSize = Buffer.byteLength(chunk);
+                downloadedSize += chunkSize;
+                if (getPercentage(downloadedSize, fileSize) >= percentage) {
+                    await editMessageText(bot, `Downloaded: ${getPercentage(downloadedSize, fileSize)}%` , editMessageOptions);
+                    percentage += 10;
+                }
+            });
+
             fileStream.on('finish', async () => {
                 fileStream.close();
-                console.log('Download Completed');
-                await bot.sendMessage(userId, 'Download Completed');
-                const readStream = fs.createReadStream(path);
-                await bot.sendVideo(userId, readStream);
 
+                console.log('Download Completed');
+                await editMessageText(bot, 'Downloaded: 100%' , editMessageOptions);
+                await editMessageText(bot, 'Uploaded: 0%' , editMessageOptions);
+                
+                const readStream = fs.createReadStream(path);
+                let uploadedSize = 0;
+                let percentage = 0;
+                readStream.on('data', async (chunk) => {
+                    const chunkSize = Buffer.byteLength(chunk);
+                    uploadedSize += chunkSize;
+
+                    if (getPercentage(uploadedSize, fileSize) >= percentage) {
+                        await editMessageText(bot, `Uploaded: ${getPercentage(uploadedSize, fileSize)}%`, editMessageOptions);
+                        percentage += 10;
+                    }
+                });
+                await bot.sendVideo(userId, readStream);
+                await editMessageText(bot, 'Uploaded: 100%', editMessageOptions);
+                await bot.deleteMessage(userId, message.message_id.toString());
                 fs.unlink(path, (err) => {
                     if (err) {
                         console.log(err);
@@ -87,3 +118,8 @@ bot
     .startPolling()
     .then(() => console.log('Bot Started'))
     .catch(() => console.error('Error cant connect'));
+
+
+const editMessageText = async (bot: TelegramBot, messageText: string, options: TelegramBot.EditMessageTextOptions) => {
+    await bot.editMessageText(messageText, options).catch(() => {});
+}
